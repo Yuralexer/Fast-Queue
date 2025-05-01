ccb8d2e48edeb32d62409d71f16226ef3cc3d957
Yuralexer <yura582005@gmail.com>
2025-05-02 01:26:05 +0300

    back: env settings has beed removed

--- fastqueue/settings.py
+++ fastqueue/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "dzf2m01!)@ts6z07wy&%8v%w11a#td1v3+i9tn+d^vn3gowgw("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
