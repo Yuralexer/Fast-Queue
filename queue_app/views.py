f025cee3a79bcae0d88e357d0dcd23a4e7390e96
Yuralexer <yura582005@gmail.com>
2025-05-04 00:24:43 +0300

    update: add queue search pages

--- queue_app/views.py
+++ queue_app/views.py
@@ -1,17 +1,35 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render
 
-# from django.shortcuts import render, redirect
+from .models import Queue
 
 
 @login_required(login_url="auth:sign-in")
 def index(request):
-    return HttpResponse("Вы авторизовались! Ура!")
+    query = request.GET.get('search_request', '')
+    if query:
+        queues = Queue.objects.filter(keyword__icontains=query)
+    else:
+        queues = Queue.objects.all()
+    return render(
+        request,
+        'queue_app/queue_search.html',
+        {
+            'user': request.user,
+            'queues': queues,
+            'search_request': query if query else None,
+        })
 
 
 @login_required(login_url="auth:sign-in")
-def queue_page_view(request):
-    return HttpResponse("Страница очереди")
+def queue_page_view(request, queue_id):
+    return render(
+        request,
+        'queue_app/queue_password_enter.html',
+        {
+            'queue': Queue.objects.get(pk=queue_id),
+        })
 
 
 @login_required(login_url="auth:sign-in")
